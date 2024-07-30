fd12a9f0e0c2b65b8200a52a3867344e86c86b44
liyizhuo <onezylee@163.com>
2024-07-30 16:34:16 +0800

    eureka注册中心

--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -19,7 +19,8 @@ public class OrderService {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
         // 2.利用RestTemplate发起http请求，查询用户
-        String url = "http://localhost:8081/user/"+order.getUserId();
+//        String url = "http://localhost:8081/user/"+order.getUserId();
+        String url = "http://userservice/user/" + order.getUserId();
         // 3.调用远程方法
         User user = restTemplate.getForObject(url, User.class);
         // 4.设置user到order
